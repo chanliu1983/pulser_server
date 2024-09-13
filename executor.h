56768a99f221d771b90bb97b78fdc9c3b5df9cb1
Lei Liu <lei.liu@autodesk.com>
2024-09-13 15:05:28 +0800

    Missing one file

--- executor.h
+++ executor.h
@@ -2,14 +2,20 @@
 #define EXECUTOR_H
 
 #include <string>
+#include "multicast.h"
 
 class Executor {
 public:
+    Executor(MulticastHandler* multicastHandler);
     void processJsonCommand(const int& fd, const std::string& jsonCommand);
+
 private:
     void connect(const int& fd, const std::string& value);
     void disconnect(const int& fd, const std::string& value);
     void send(const int& fd, const std::string& value, const std::string& target);
+
+private:
+    MulticastHandler* multicastHandler_;
 };
 
 #endif // EXECUTOR_H
